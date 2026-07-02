b4ac3e340f9546e78edc9299800f58923209c611
pooja-arun-bhosale <poojaabhosale23@gmail.com>
2026-07-02 20:20:37 +0530

    Fix fallback URL in seed test for environment variable

--- tests/seed.spec.ts
+++ tests/seed.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test.describe("Test group", () => {
   test("seed", async ({ page }) => {
-    await page.goto(process.env.URL!, {
+    await page.goto(process.env.URL||"https://opensource-demo.orangehrmlive.com/web/index.php/auth/login", {
       waitUntil: "domcontentloaded",
     });
   });
