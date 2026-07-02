465d5a6d417686d8ffc732b91d880c2fd803b12a
pooja-arun-bhosale <poojaabhosale23@gmail.com>
2026-07-02 19:48:16 +0530

    Add dotenv dependency and configure environment variables; update TypeScript settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
+import dotenv from 'dotenv';
+dotenv.config();
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -39,15 +32,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
--- tests/seed.spec.ts
+++ tests/seed.spec.ts
@@ -1,7 +1,9 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test.describe('Test group', () => {
-  test('seed', async ({ page }) => {
-    // generate code here.
+test.describe("Test group", () => {
+  test("seed", async ({ page }) => {
+    await page.goto(process.env.URL!, {
+      waitUntil: "domcontentloaded",
+    });
   });
 });
